89d95f9c1f8fe33782e3f0d6062d075f314fd1d8
Alison Stuart <hax@Alisons-MacBook-Air.local>
2017-02-28 14:48:45 -0500

    fixed some styling issues

--- js/app.js
+++ js/app.js
@@ -7,13 +7,3 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.carousel-notFullWidth').carousel();
 });
-
-function makeSlide() {
-    document.getElementById('hiRunner').className = "slider";
-    document.getElementById('topBox').setAttribute("class", "lightBlue topBox");
-}
-
-function contactMe() {
-    makeSlide();
-    document.getElementById('choiceBox').innerHTML = ("<div id=\"contactChoice\"><p id=\"hearBack\">I look forward to hearing from you!</p><a href=\"https://www.linkedin.com/in/alison-n-stuart\"><img src=\"./imgs/linkedIn.png\"></a><a href=\"mailto:alisonnicolestuart@gmail.com?Subject=Saw%20Your%20Website\"><img src=\"./imgs/gmail.gif\"></a></div>");
-}
